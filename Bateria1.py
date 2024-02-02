3b60ce5a09eabb8a16bbf2e99ec1b1fd9e7d2734
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-02-02 13:50:15 +0100

    Update Bateria1.py

--- Bateria1.py
+++ Bateria1.py
@@ -7,3 +7,43 @@ num2 = float(input("Ingresa el segundo número: "))
 suma = num1 + num2
 
 print(f"La suma de {num1} y {num2} es: {suma}")23
+
+
+
+# Programa que pide un número al usuario y retorna su cuadrado
+
+numero = float(input("Ingresa un número: "))
+
+cuadrado = numero ** 2
+
+print(f"El cuadrado de {numero} es: {cuadrado}")
+
+
+
+# Programa que pide al usuario su nombre y retorna un saludo específico
+
+nombre = input("Ingresa tu nombre: ")
+
+saludo = "Hola Ilyas"
+
+print(saludo)
+
+
+
+# Programa que pide al usuario un peso en kilogramos y retorna el mismo peso en libras
+
+peso_kg = float(input("Ingresa el peso en kilogramos: "))
+
+peso_lbs = peso_kg * 2.20462
+
+print(f"El peso de {peso_kg} kilogramos es equivalente a {peso_lbs} libras.")
+
+
+
+# Programa que pide al usuario una temperatura en grados Celsius y retorna la temperatura en grados Fahrenheit
+
+temperatura_celsius = float(input("Ingresa la temperatura en grados Celsius: "))
+
+temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+
+print(f"La temperatura de {temperatura_celsius} grados Celsius es equivalente a {temperatura_fahrenheit} grados Fahrenheit.")
