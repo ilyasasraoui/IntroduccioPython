6b5932b0ad22548ff969fd34ff0f3d9757da5f0c
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-02-02 13:43:21 +0100

    Update Bateria1.py

--- Bateria1.py
+++ Bateria1.py
@@ -1,1 +1,9 @@
-Programa que pregunta dos números a l'usuari i calcula la seva suma.
+# Programa que pide dos números al usuario y calcula su suma
+
+num1 = float(input("Ingresa el primer número: "))
+
+num2 = float(input("Ingresa el segundo número: "))
+
+suma = num1 + num2
+
+print(f"La suma de {num1} y {num2} es: {suma}")23
