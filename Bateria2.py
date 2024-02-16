3be79d17d8bcc6734b21d60892f25445a9859dea
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-02-16 14:56:08 +0100

    Update Bateria2.py

--- Bateria2.py
+++ Bateria2.py
@@ -1,1 +1,5 @@
-
+# Programa que pregunta el nom de l'usuari i només dona la benvinguda si l'usuari es diu "Amilcar" o "Sila".
+nom_usuari = input("Introdueix el teu nom: ")
+if nom_usuari == "Amilcar" or nom_usuari == "Sila":
+    print("Benvingut, " + nom_usuari + "!")
+# Programa que pregunta un número a l'usuari i diu si és major o menor que 100.
