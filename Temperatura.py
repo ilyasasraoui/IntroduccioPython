3606b61108bb7391817a706f4b021a1c2ea270dd
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-03-20 09:31:18 +0100

    Create Temperatura.py

---
+++ Temperatura.py
@@ -0,0 +1,7 @@
+edat = int(input("Quina edat tens?"))
+if edat < 18:
+    print("No pots passar encara no ets major d'edat")
+elif edat == 18:
+    print("pots passar ets major de edat")  
+else:
+    print("Venga passa")
