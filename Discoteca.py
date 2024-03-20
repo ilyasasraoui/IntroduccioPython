230bd445d8a1b886438c8aaf0340e86a5c1176db
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-03-20 09:17:31 +0100

    Create Discoteca.py

---
+++ Discoteca.py
@@ -0,0 +1,7 @@
+edat = int(input("Quina edat tens?"))
+if edat < 18:
+    print("No pots passar encara no ets major d'edat")
+elif edat == 18:
+    print("Passas justet eh vinga passa")  
+else:
+    print("Passa endevant")
