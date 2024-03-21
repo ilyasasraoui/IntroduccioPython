2b6e228ad66e0fce5477bce805ee31f230ef2726
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-03-21 13:12:10 +0100

    Update Bateria1-4.py

--- Bateria1-4.py
+++ Bateria1-4.py
@@ -1,1 +1,7 @@
+# Programa que demana un pes en kg a l'usuari i retorna el mateix pes convertit en lliures.
 
+peso_kg = float(input("Ingresa el peso en kilogramos: "))
+
+peso_lbs = peso_kg * 2.20462
+
+print(f"El peso de {peso_kg} kilogramos es equivalente a {peso_lbs} libras.")
