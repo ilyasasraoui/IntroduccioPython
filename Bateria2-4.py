708fdc08eb30a67c1dc697e75cf5281599e13eef
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-03-21 13:19:05 +0100

    Update Bateria2-4.py

--- Bateria2-4.py
+++ Bateria2-4.py
@@ -1,1 +1,12 @@
-
+nota = input("Quina es la teva nota? ")
+nota = round(float(nota))
+if nota == 9 or nota == 10:
+    print("La teva nota es un Exel·lent")
+elif nota == 7 or nota == 8:
+    print("La teva nota es Notable")
+elif nota == 6:
+    print("La teva nota es Bé")
+elif nota == 5:
+    print("La teva nota es un Suficient")
+elif nota <= 4:
+    print("La teva nota es un Insuficient ")
