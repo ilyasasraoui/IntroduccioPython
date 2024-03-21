c659222988e6c5de7db931fec9c5d5faaf8eef67
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-03-21 13:12:50 +0100

    Update Bateria1-5.py

--- Bateria1-5.py
+++ Bateria1-5.py
@@ -1,1 +1,6 @@
+# Programa que demana una temperatura en graus Celsius a l'usuari i retorna la temperatura en graus Fahrenheit.
+temperatura_celsius = float(input("Ingresa la temperatura en grados Celsius: "))
 
+temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32
+
+print(f"La temperatura de {temperatura_celsius} grados Celsius es equivalente a {temperatura_fahrenheit} grados Fahrenheit.")
