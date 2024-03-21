c177a730ae8dffee30d9135d21e78594c4c8b08e
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-03-21 13:10:58 +0100

    Update Bateria1-2.py

--- Bateria1-2.py
+++ Bateria1-2.py
@@ -1,1 +1,7 @@
+# Programa que pregunta un número i retorna el seu quadrat.
 
+numero = float(input("Ingresa un número: "))
+
+cuadrado = numero ** 2
+
+print(f"El cuadrado de {numero} es: {cuadrado}")
