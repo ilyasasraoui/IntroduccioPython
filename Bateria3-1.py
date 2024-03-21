453436971013f97eab3925043b73372451d97b9a
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-03-21 13:21:46 +0100

    Update Bateria3-1.py

--- Bateria3-1.py
+++ Bateria3-1.py
@@ -1,1 +1,5 @@
-
+numero = int(input("introdueix un número: "))
+print(f"Taula de multiplicar del {numero}:")
+for i in range(1, 11):
+    resultat = numero * i
+    print(f"{numero} x {i} = {resultat}")
