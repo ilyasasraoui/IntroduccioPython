8acccab1930095ee73cf6ed90d0e7b0c549a21c3
ilyasasraoui <158280786+ilyasasraoui@users.noreply.github.com>
2024-03-21 13:14:40 +0100

    Update Bateria2-2.py

--- Bateria2-2.py
+++ Bateria2-2.py
@@ -1,1 +1,7 @@
-
+numero = input("Diguem un numero: ")
+if int(numero) > 100:
+    print("Aquest numero es major a 100")
+elif int(numero) == 100:
+    print("Aquest numero es igual a 100")
+else:
+    print ("Aquest numero es menor a 100")
